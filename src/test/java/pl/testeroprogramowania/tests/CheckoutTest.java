ef683ccfb13bada89178d0d3bf31f6e11cfd677d
DanielLachacz <32907234+DanielLachacz@users.noreply.github.com>
2023-03-28 17:42:01 +0200

    Add tests and page objects

---
+++ src/test/java/pl/testeroprogramowania/tests/CheckoutTest.java
@@ -0,0 +1,32 @@
+package pl.testeroprogramowania.tests;
+
+import com.fasterxml.jackson.databind.deser.impl.ObjectIdReader;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pl.testeroprogramowania.models.Customer;
+import pl.testeroprogramowania.pages.HomePage;
+import pl.testeroprogramowania.pages.OrderDetailsPage;
+
+public class CheckoutTest extends BaseTest {
+
+    @Test
+    public void checkoutTest() {
+        Customer customer = new Customer();
+
+
+        OrderDetailsPage orderDetailsPage =
+                new HomePage(driver)
+                .openShopPage()
+                .openProduct("Java Selenium WebDriver")
+                .addProductToCart()
+                .viewCart()
+                .openAddressDetails()
+                .fillAddressDetails(customer,"Some random comment");
+
+        Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(), "Thank you. Your order has been received");
+        Assert.assertEquals(orderDetailsPage.getProductName().getText(), "Java Selenium WebDriver x 1");
+    }
+
+
+}
